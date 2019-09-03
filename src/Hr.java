e880b448928ee7e1f3d5ebefbe7e5d1fa0e4fc14
Deelawor. Sadiyah <52469086+deelawor@users.noreply.github.com>
2019-09-03 16:58:32 +0400

    Update Hr.java

--- src/Hr.java
+++ src/Hr.java
@@ -16,7 +16,9 @@ public class Hr {
 		//Creating object student and lecturer.
 		Student jSmith = new Student ("J1209950078563", "John Smith", 173, 67, "JBDA000", "Big Data Analyis", "John Donald");
 		Lecturer max = new Lecturer ("M2905940078985", "Max Thomson", 189, 76, "Maths", "PML", 750, 1.5);
+		max.setAcademia("CTI");
 		
+		System.out.println((max.getNid()).equals(jSmith.getNid()));
 		System.out.println(jSmith.calculateBmi());
 		System.out.println("Name of student "+ jSmith.getName() + " has a salary of : " +jSmith.calculateSalary(0, 0) );
 		System.out.println(jSmith.toString());
